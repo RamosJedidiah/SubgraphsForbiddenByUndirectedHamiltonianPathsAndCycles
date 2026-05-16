091bba3a4b7c4a08a6df3a62f9208489fcf1ec49
Jedidiah Ramos <50191499+RamosJedidiah@users.noreply.github.com>
2026-05-17 03:25:54 +0800

    Update WklSubgraphConditionVerifierForEdgeList.java

--- WklSubgraphConditionVerifierForEdgeList.java
+++ WklSubgraphConditionVerifierForEdgeList.java
@@ -483,7 +483,7 @@ public class WklSubgraphConditionVerifierForEdgeList {
                     BCC2of[BCCvertex] = i;
                 } else {
                     // Vertex is shared by at least 3 biconnected components
-                    System.out.println("All W_k,l subgraph conditions are violated because vertex " + BCCvertex + " has criticality at least 3");
+                    System.out.println("The W_k,k subgraph condition is violated because vertex " + BCCvertex + " has criticality at least 3");
                     return true;
                 }
             }
